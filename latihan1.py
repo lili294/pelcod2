89012cc31aec35cfb2aeba8b701a6d1bb5c96887
lili294 <143860033+lili294@users.noreply.github.com>
2023-09-02 10:59:33 +0700

    Add files via upload
    upload file

---
+++ latihan1.py
@@ -0,0 +1,20 @@
+nama_saya = "nama saya lili"
+print(nama_saya, "bertipe", type(nama_saya)
+
+umur = 19
+print(umur, "bertipe", type(umur))
+
+ipk = 4.00
+print(ipk, "bertipe", type(ipk))
+
+menikah = false
+print("menikah?",menikah, "bertipe", type(menikah))
+
+matkul_diambil = ["alpro", "sig"]
+print("matkul : ",matkul_diambil, "bertipe", type(matkul_diambil))
+
+matkul_diambil2 = ("web","pemvis")
+print("Matkul Lain: ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+matkul_diambil3 = {"alpro", "aLpro", "pemweb", "data science", "data science"}
+print("matkul Lain-Lain": ",matkul_diambil3, "bertipe", type(matkul_diambil3))
